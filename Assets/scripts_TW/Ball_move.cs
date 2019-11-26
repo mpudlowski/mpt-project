9c230f25c4145eecb42d055ba95fcd45377f2203
michalp <33500671+mpudlowski@users.noreply.github.com>
2019-11-26 13:12:28 +0100

    arrow

--- Assets/scripts_TW/Ball_move.cs
+++ Assets/scripts_TW/Ball_move.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Ball_move : MonoBehaviour
 {
-	public float min_z = -60f, max_z = 60f;
-	public float rotate_speed = 5f;
-	
-	private float rotate_angle;
-	private bool rotate_right;
-	
-	private bool can_rotate;
-	
-	public float move_speed = 3f;
-	private float start_move_speed;
-	
-	public float max_y = 4.8f;
-	private float start_y;
-	private bool move_up;
-	
-	//for ball move
-	//line renderer
-   // Start is called before the first frame update
+    public float min_z = -60f, max_z = 60f;
+    public float rotate_speed = 5f;
+
+    private float rotate_angle;
+    private bool rotate_right;
+
+    private bool can_rotate;
+
+    public float move_speed = 3f;
+    private float start_move_speed;
+
+    public float max_y = 4.8f;
+    private float start_y;
+    private bool move_up;
+
+    //for ball move
+    //line renderer
+    // Start is called before the first frame update
     void Start()
     {
         start_y = transform.position.y;
-		start_move_speed = move_speed;
-		
-		can_rotate = true;
+        start_move_speed = move_speed;
+
+        can_rotate = true;
     }
 
     // Update is called once per frame
@@ -35,39 +35,39 @@ public class Ball_move : MonoBehaviour
     {
         Rotate();
     }
-	
-	void Rotate()
-	{
-		if(!can_rotate)
-			return;
-		
-		if(rotate_right)
-		{
-			rotate_angle += rotate_speed * Time.deltaTime;
-		}
-		else
-		{
-			rotate_angle -= rotate_speed * Time.deltaTime;
-		}
-		
-		transform.rotation = Quaternion.AngleAxis(rotate_angle, Vector3.forward);
-		
-		if(rotate_angle >= max_z)
-		{
-			rotate_right = false;
-		} 
-		else if(rotate_angle <= min_z)
-		{
-			rotate_right = true;
-		}
-		
-	
-	
-	
-	
-	
-	}
-	
-	
-	
+
+    void Rotate()
+    {
+        if (!can_rotate)
+            return;
+
+        if (rotate_right)
+        {
+            rotate_angle += rotate_speed * Time.deltaTime;
+        }
+        else
+        {
+            rotate_angle -= rotate_speed * Time.deltaTime;
+        }
+
+        transform.rotation = Quaternion.AngleAxis(rotate_angle, Vector3.forward);
+
+        if (rotate_angle >= max_z)
+        {
+            rotate_right = false;
+        }
+        else if (rotate_angle <= min_z)
+        {
+            rotate_right = true;
+        }
+
+
+
+
+
+
+    }
+
+
+
 }
